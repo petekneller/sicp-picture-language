3639bf4cc74a923de00605a303874c3f99d04ecf
Pete Kneller <mrkneller@hotmail.com>
2014-01-17 17:52:55 +0000

    can now draw a basic line and image drawn onto the swing panel

--- sicp-picture-library/src/picture/app.clj
+++ sicp-picture-library/src/picture/app.clj
@@ -1,16 +1,26 @@
 (ns picture.app
   (:gen-class)
   (:import [javax.swing SwingUtilities JFrame JPanel BorderFactory]
-           [java.awt Dimension Color]))
+           [java.awt Dimension Color]
+           [javax.imageio ImageIO])
+  (:require [clojure.java.io :as io]))
 
 (defn newPanel []
   (let [panel (proxy [JPanel] []
-                (getPreferredSize [] (new Dimension 250 200))
+                (getPreferredSize [] (new Dimension 500 500))
                 (paintComponent [gfx]
                   (do (proxy-super paintComponent gfx)
-                      (.drawString gfx "this is my custom panel!" 10 20))))
-        _ (.setBorder panel (BorderFactory/createLineBorder Color/black))]
-    panel))
+                      ;(.drawString gfx "this is my custom panel!" 10 20)
+                      (.setColor gfx Color/BLACK)
+                      (.drawLine gfx 10 10 100 30)
+                      (.drawImage gfx
+                                  (ImageIO/read (io/resource "roger.gif"))
+                                  50 50 400 400 ; dest rect on panel
+                                  0 0 180 180 ; src rect in image
+                                  nil))))]
+    (do
+      (.setBorder panel (BorderFactory/createLineBorder Color/black))
+      panel)))
 
 (defn run-main []
   (SwingUtilities/invokeLater
