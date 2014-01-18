111773678b7cf630389c288b7cc3afa0af243281
Pete Kneller <mrkneller@hotmail.com>
2014-01-18 00:29:28 +0000

    working on the wave pic; need a draw-polyline

--- sicp-picture-library/src/picture/examples.clj
+++ sicp-picture-library/src/picture/examples.clj
@@ -3,13 +3,14 @@
   (:require [clojure.java.io :as io]
             [picture.swing :as swing]))
 
-(defn draw-roger [destRect]
+(defn roger [destRect]
   (swing/draw-image (io/resource "roger.gif") destRect))
 
-(def basic-picture
+(def wave
   (swing/do-renderers
       (swing/draw-line {:x 0 :y 100} {:x 150 :y 350})
       (swing/draw-line {:x 150 :y 350} {:x 300 :y 300})
       (swing/draw-line {:x 300 :y 300} {:x 350 :y 300})
-      (draw-roger {:top-left  {:x 50 :y 50}
-                 :bot-right {:x 400 :y 400}})))
+      (swing/draw-line {:x 350 :y 300} {:x 325 :y 150})
+      (swing/draw-line {:x 325 :y 150} {:x 350 :y 0})
+      ))
--- sicp-picture-library/src/picture/swing.clj
+++ sicp-picture-library/src/picture/swing.clj
@@ -18,14 +18,17 @@
 (defn draw-line [from to]
   "from and to are coords specified in 'user' space, which begins at the top-left
   and extends to 1000 in both axes"
-  (fn [gfx]
-    (.drawLine gfx (:x from) (:y from) (:x to) (:y to))))
+  (fn [gfx panel-width panel-height]
+    (let [map-fn (partial map-point-user-to-panel-space panel-width panel-height)
+          panel-from (map-fn from)
+          panel-to (map-fn to)]
+      (.drawLine gfx (:x panel-from) (:y panel-from) (:x panel-to) (:y panel-to)))))
 
 
 (defn draw-image [^URL res destRect]
   "the corners of destRect are coords specified in 'user' space, which begins at the top-left
   and extends to 1000 in both axes"
-  (fn [gfx]
+  (fn [gfx panel-width panel-height]
     (let [image (ImageIO/read res)]
       (.drawImage gfx
                   image
@@ -39,26 +42,28 @@
 
 (defn followed-by [first-renderer second-renderer]
   "creates a renderer that sequences its two arguments one after the other"
-  (fn [gfx]
+  (fn [gfx panel-width panel-height]
     (do
-      (first-renderer gfx)
-      (second-renderer gfx))))
+      (first-renderer gfx panel-width panel-height)
+      (second-renderer gfx panel-width panel-height))))
 
 
 (defn do-renderers [& renderers]
-  (fn [gfx]
-    (doall (map #(% gfx) renderers))))
+  (fn [gfx panel-width panel-height]
+    (doall (map #(% gfx panel-width panel-height) renderers))))
 
 
 (defn new-jpanel [on-paint]
   " on-paint: gfx -> nil"
-  (let [panel (proxy [JPanel] []
-                (getPreferredSize [] (new Dimension 500 500))
+  (let [panel-width 500
+        panel-height 500
+        panel (proxy [JPanel] []
+                (getPreferredSize [] (new Dimension panel-width panel-height))
                 (paintComponent [gfx]
                   (do (proxy-super paintComponent gfx)
                       (.setColor gfx Color/BLACK)
                       ;(.drawString gfx "this is my custom panel!" 10 20)
-                      (on-paint gfx))))]
+                      (on-paint gfx panel-width panel-height))))]
     (do
       (.setBorder panel (BorderFactory/createLineBorder Color/black))
       panel)))
