dcc4c5cd52827b9d9faa2175a4979e3d2fc8a006
Pete Kneller <mrkneller@hotmail.com>
2014-01-19 11:40:32 +0000

    finished the wave example drawing

--- sicp-picture-library/src/picture/examples.clj
+++ sicp-picture-library/src/picture/examples.clj
@@ -7,9 +7,26 @@
   (swing/draw-image (io/resource "roger.gif") {:top-left {:x 0 :y 0} :bot-right {:x 1000 :y 1000}}))
 
 (def wave
-  (swing/draw-polyline {:x 0 :y 100}
-                       {:x 150 :y 350}
-                       {:x 300 :y 300}
-                       {:x 350 :y 300}
-                       {:x 325 :y 150}
-                       {:x 350 :y 0}))
+  (swing/do-renderers
+    (swing/draw-polyline {:x 0 :y 100}
+                         {:x 150 :y 350}
+                         {:x 300 :y 300}
+                         {:x 350 :y 300}
+                         {:x 325 :y 150}
+                         {:x 350 :y 0})
+    (swing/draw-polyline {:x 600 :y 0}
+                         {:x 625 :y 150}
+                         {:x 600 :y 300}
+                         {:x 750 :y 300}
+                         {:x 1000 :y 650})
+    (swing/draw-polyline {:x 1000 :y 800}
+                         {:x 625 :y 500}
+                         {:x 700 :y 1000})
+    (swing/draw-polyline {:x 625 :y 1000}
+                         {:x 500 :y 700}
+                         {:x 375 :y 1000})
+    (swing/draw-polyline {:x 300 :y 1000}
+                         {:x 325 :y 500}
+                         {:x 300 :y 350}
+                         {:x 150 :y 500}
+                         {:x 0 :y 250})))
