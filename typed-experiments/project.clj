000993ab695c8b06b36e69f33d7c4025eb2ead64
Poita79 <mrkneller@hotmail.com>
2014-01-26 20:15:04 +0000

    cleaning up all but the picture language

--- typed-experiments/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject typed-experiments "dev"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.typed "0.2.19"]] 
-			; 0.2.21 is the latest version, but has a typing of Map that doesnt work well
-  :main typed-experiments)
